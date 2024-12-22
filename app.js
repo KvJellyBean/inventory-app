ab85035f274487c4725babe405be9fc4c57add2e
KvJellyBean <kevinnatan91@gmail.com>
2024-12-22 10:04:22 +0700

    fix: add eeror page and fix typo

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const categories = require("./routes/categories");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 
-app.set("viiews", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
@@ -16,6 +16,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", home);
 app.use("/items", items);
 app.use("/categories", categories);
+app.get("/errorPage", (req, res) => {
+  const errorMessage = req.query.error || "An unexpected error occurred.";
+  res.render("errorPage", { error: errorMessage });
+});
+app.use(function (req, res) {
+  res.status(404).render("errorPage");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
